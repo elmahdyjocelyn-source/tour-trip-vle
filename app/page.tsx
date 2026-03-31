78a20643536f15712ef068d0c8d1ec878b148792
Jocelyn Elmahdy <elmahdyjocelyn@gmail.com>
2026-03-31 12:54:18 +0800

    fix image paths in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 const tours = [
   {
     name: "Sirao Flower Garden",
-    images: ["/images/sirao/1.jpg","/images/sirao/2.jpg","/images/sirao/3.jpg"]
+    images: [
+      "/images/sirao/sirao-1.jpg",
+      "/images/sirao/sirao-2.jpg",
+      "/images/sirao/sirao-3.jpg"
+    ]
   },
   {
     name: "Whale Shark Dive",
-    images: ["/images/whaleshark-dive/1.jpg"]
+    images: [
+      "/images/whaleshark-dive/whaleshark-dive.jpg"
+    ]
   },
   {
     name: "Moalboal Sardine Dive",
-    images: ["/images/moalboal-sardine-tour/1.jpg"]
+    images: [
+      "/images/moalboal-sardine-tour/sardine-dive.jpg"
+    ]
   },
   {
     name: "Thousand Roses",
-    images: ["/images/thousand-roses/1.jpg"]
+    images: [
+      "/images/thousand-roses/thousand-roses.jpg"
+    ]
   }
 ];
