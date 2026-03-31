661d6cbda4a495da99c94983c0347b13d0ba3169
Jocelyn Elmahdy <elmahdyjocelyn@gmail.com>
2026-03-31 12:29:55 +0800

    update tours content and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
 const tours = [
   {
-    name: "moalboal-sardine-tour",
-    images: [
-      "/images/moalboal-sardine-tour/1.jpg"
-    ]
+    name: "Sirao Flower Garden",
+    images: ["/images/sirao/1.jpg","/images/sirao/2.jpg","/images/sirao/3.jpg"]
   },
   {
-    name: "whaleshark-dive",
-    images: [
-      "/images/whaleshark-dive/1.jpg"
-    ]
+    name: "Whale Shark Dive",
+    images: ["/images/whaleshark-dive/1.jpg"]
   },
   {
-    name: "sirao",
-    images: [
-      "/images/sirao/sirao-1.jpg"
-    ]
+    name: "Moalboal Sardine Dive",
+    images: ["/images/moalboal-sardine-tour/1.jpg"]
+  },
+  {
+    name: "Thousand Roses",
+    images: ["/images/thousand-roses/1.jpg"]
   }
 ];
--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,24 +1,28 @@
-export default function Tours() {
-  return (
-    <main style={{padding:"40px"}}>
-
-      <h1>Cebu Tours</h1>
-
-      <h2>sardine dive</h2>
-      <img src="/images/moalboal-sardine-tour/sardino-dive.jpg" width="300" />
-
-      <h2>sirao</h2>
-      <img src="/images/sirao/sirao-1.jpg" width="300" />
-      <img src="/images/sirao/sirao-2.jpg" width="300" />
-      <img src="/images/sirao/sirao-3.jpg" width="300" />
-
-      <h2>Oslob Whale Shark</h2>
-      <img src="/images/whaleshark-dive/whaleshark-dive.jpg" width="300" />
-
-      <h2>thousand roses</h2>
-      <img src="/images/thoursand-roses/thouand-roses.jpg" width="300" />
-
-
-    </main>
-  );
-}
+const tours = [
+  {
+    name: "Sirao Flower Garden",
+    images: [
+      "/images/sirao/sirao-1.jpg",
+      "/images/sirao/sirao-2.jpg",
+      "/images/sirao/sirao-3.jpg"
+    ]
+  },
+  {
+    name: "Whale Shark Dive",
+    images: [
+      "/images/whaleshark-dive/whaleshark-dive.jpg"
+    ]
+  },
+  {
+    name: "Moalboal Sardine Dive",
+    images: [
+      "/images/moalboal-sardine-tour/sardine-dive.jpg"
+    ]
+  },
+  {
+    name: "Thousand Roses",
+    images: [
+      "/images/thousand-roses/thousand-roses.jpg"
+    ]
+  }
+];
